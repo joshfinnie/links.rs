807d17dca933c0a47c88c68f461bb7ad6f402a6c
Josh Finnie <josh.finnie@peopledatalabs.com>
2026-01-11 19:44:57 -0500

    Moving to Astro.
    This was hyper complex, made to be more of a package than just a links
    website. It wasn't worth it. I moved to Astro, it builds to a single
    static file. perfect

--- src/components/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod avatar;
-pub mod button;
-pub mod header;
-pub mod utils;
---
+++ src/data/links.ts
@@ -0,0 +1,17 @@
+export const socials = [
+  { name: 'GitHub', url: 'https://github.com/joshfinnie', icon: 'mingcute:github-line', color: 'hover:text-white hover:bg-[#24292e]' },
+  { name: 'LinkedIn', url: 'https://linkedin.com/in/joshfinnie', icon: 'mingcute:linkedin-line', color: 'hover:text-white hover:bg-[#0077b5]' },
+  { name: 'BlueSky', url: 'https://bsky.app/profile/joshfinnie.dev', icon: 'mingcute:bluesky-social-line', color: 'hover:text-white hover:bg-[#0085ff]' },
+  { name: 'Twitch', url: 'https://twitch.tv/joshfinnie', icon: 'mingcute:twitch-line', color: 'hover:text-white hover:bg-[#9146ff]' },
+  { name: 'YouTube', url: 'https://youtube.com/@joshfinnie', icon: 'mingcute:youtube-line', color: 'hover:text-white hover:bg-[#ff0000]' },
+];
+
+export const personalLinks = [
+  { label: 'joshfinnie.com', url: 'https://www.joshfinnie.com', icon: 'mingcute:cloud-2-line' },
+  { label: 'josh@jfin.us', url: 'mailto:josh@jfin.us', icon: 'mingcute:mail-line' },
+];
+
+export const bio = {
+  name: "Josh Finnie",
+  description: "Senior engineer building things with code",
+}
